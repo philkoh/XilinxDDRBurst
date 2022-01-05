02b357f3ecd173449536c1af2c8d0714236db41a
GooseEnt <gooseenterprisesllc@gmail.com>
2022-01-04 23:23:46 -0500

    added delay to write bit0, seems that all the bits are working except bit0

--- bl.vhd
+++ bl.vhd
@@ -155,7 +155,8 @@ signal nextRequestedDataToWrite : philArr;
 	
 	signal dataWaitingForOutput : std_logic_vector(15 downto 0);
 	signal nextDataWaitingForOutput : std_logic_vector(15 downto 0);
-
+	signal delayedDataForOutput : std_logic_vector(15 downto 0);
+	
 
 	signal tristateData : std_logic := '1';
 	signal tristateDataFromDDR : std_logic := '1';
@@ -223,7 +224,37 @@ your_instance_name : PhilClock
 
 
 
-
+IODELAY2_inst : IODELAY2
+generic map (
+COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
+DATA_RATE => "SDR", -- "SDR" or "DDR"
+DELAY_SRC => "ODATAIN", -- "IO", "ODATAIN" or "IDATAIN"
+IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
+IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
+IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
+-- or "DIFF_PHASE_DETECTOR"
+IDELAY_VALUE => 0, -- Amount of taps for fixed input delay (0-255)
+ODELAY_VALUE => 0, -- Amount of taps fixed output delay (0-255)
+SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
+SIM_TAPDELAY_VALUE => 75 -- Per tap delay used for simulation in ps
+)
+port map (
+--BUSY => BUSY, -- 1-bit output: Busy output after CAL
+--DATAOUT => DATAOUT, -- 1-bit output: Delayed data output to ISERDES/input register
+--DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
+DOUT => delayedDataForOutput(0), -- 1-bit output: Delayed data output
+--TOUT => TOUT, -- 1-bit output: Delayed 3-state output
+CAL => '0', -- 1-bit input: Initiate calibration input
+CE => '0', -- 1-bit input: Enable INC input
+CLK => '0', -- 1-bit input: Clock input
+IDATAIN => '0', -- 1-bit input: Data input (connect to top-level port or I/O buffer)
+INC => '0', -- 1-bit input: Increment / decrement input
+IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
+IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
+ODATAIN => dataWaitingForOutput(0), -- 1-bit input: Output data input from output register or OSERDES2.
+RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
+T => '0' -- 1-bit input: 3-state input signal
+);
 
 
 
@@ -358,7 +389,9 @@ I => clk125MHz -- Buffer input
 		if dqsTristate = '1' then
 			dataPort (15 downTo 0) <= (15 downTo 0 => 'Z');	
 		else
-			dataPort <= dataWaitingForOutput;
+			dataPort(0) <= delayedDataForOutput(0);
+			dataPort(15 downto 1) <= dataWaitingForOutput (15 downto 1);
+			
 		end if;
 	
 		if clockEnableWrite = '1' and writeRequest = '1' then --write data, and pull down the stack of registers
