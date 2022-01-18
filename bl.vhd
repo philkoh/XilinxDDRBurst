6299edb21366ad8ebfbe8691a952eca79d4a15f0
GooseEnt <gooseenterprisesllc@gmail.com>
2022-01-17 22:40:29 -0500

    Revert "tried to add a selectIO port IP block, but didn't work"
    This reverts commit ba3751401ffb4e52ee4f89a6f35a70ad53521f90.

--- bl.vhd
+++ bl.vhd
@@ -136,29 +136,6 @@ END COMPONENT;
 	
 
 
-------------- Begin Cut here for COMPONENT Declaration ------ COMP_TAG
-component PhilPorts
-generic
- (-- width of the data for the system
-  sys_w       : integer := 16;
-  -- width of the data for the device
-  dev_w       : integer := 16);
-port
- (
-  DATA_TO_AND_FROM_PINS   : inout std_logic_vector(sys_w-1 downto 0);
-  DATA_IN_TO_DEVICE       : out   std_logic_vector(dev_w-1 downto 0);
-  -- From the device out to the system
-  DATA_OUT_FROM_DEVICE    : in    std_logic_vector(dev_w-1 downto 0);
-
-  TRISTATE_OUTPUT         : in    std_logic;
--- Clock and reset signals
-  CLK_IN                  : in    std_logic;                    -- Single ended Fast clock from IOB
-  CLK_OUT                 : out   std_logic;
-  IO_RESET                : in    std_logic);                   -- Reset signal for IO circuit
-end component;
-
--- COMP_TAG_END ------ End COMPONENT Declaration ------------
-
 
 
 
@@ -521,55 +498,225 @@ your_instance_name : PhilClock
 
 
 
+IODELAY2_inst0 : IODELAY2
+generic map (
+COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
+DATA_RATE => "SDR", -- "SDR" or "DDR"
+DELAY_SRC => "IO", -- "IO", "ODATAIN" or "IDATAIN"
+IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
+IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
+IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
+-- or "DIFF_PHASE_DETECTOR"
+IDELAY_VALUE => 40, -- Amount of taps for fixed input delay (0-255)
+ODELAY_VALUE => 40, -- Amount of taps fixed output delay (0-255)
+--IDELAY:
+-- 0 gives no change
+-- 60 gives no change
+-- 90 gives no change
+-- 95 gives no change
+-- 100 is flickering borderline 1 bit delay
+-- 105 gives 1 bit delay
+-- 120 gives 1 bit delay
+
+--ODELAY:
+-- 0 gives no change
+-- 1 gives 1 bit delay
+-- 5 gives 1 bit delay
+-- 10 gives 1 bit delay
+-- 20 gives 1 bit delay
+-- 40 gives  1 bit delay
+-- 80  gives  1 bit delay
+-- 120 gives 1 bit delay
+-- 125 gives 2 bit delay
+-- 130 gives 2 bit delay
+-- 140 gives 2 bit delay
+-- 160 gives 2 bit delay
+SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
+SIM_TAPDELAY_VALUE => 75) -- Per tap delay used for simulation in ps
+port map (
+--BUSY => BUSY, -- 1-bit output: Busy output after CAL
+DATAOUT => delayedDataPort(0), -- 1-bit output: Delayed data output to ISERDES/input register
+--DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
+DOUT => delayedDataForOutput(0), -- 1-bit output: Delayed data output
+--TOUT => TOUT, -- 1-bit output: Delayed 3-state output
+CAL => '0', -- 1-bit input: Initiate calibration input
+CE => '0', -- 1-bit input: Enable INC input
+CLK => '0', -- 1-bit input: Clock input
+IDATAIN => dataPort(0), -- 1-bit input: Data input (connect to top-level port or I/O buffer)
+INC => '0', -- 1-bit input: Increment / decrement input
+IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
+IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
+ODATAIN => fastWriteData(0),--dataAssertedToOutput(0), -- 1-bit input: Output data input from output register or OSERDES2.
+RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
+T => slowdqsTristate -- 1-bit input: 3-state input signal
+);
 
 
 
 
+IODELAY2_inst1 : IODELAY2
+generic map (
+COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
+DATA_RATE => "SDR", -- "SDR" or "DDR"
+DELAY_SRC => "IO", -- "IO", "ODATAIN" or "IDATAIN"
+IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
+IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
+IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
+-- or "DIFF_PHASE_DETECTOR"
+IDELAY_VALUE => 40, -- Amount of taps for fixed input delay (0-255)
+ODELAY_VALUE => 40, -- Amount of taps fixed output delay (0-255)
+--IDELAY:
+-- 0 gives no change
+-- 60 gives no change
+-- 90 gives no change
+-- 95 gives no change
+-- 100 is flickering borderline 1 bit delay
+-- 105 gives 1 bit delay
+-- 120 gives 1 bit delay
+
+--ODELAY:
+-- 0 gives no change
+-- 1 gives 1 bit delay
+-- 5 gives 1 bit delay
+-- 10 gives 1 bit delay
+-- 20 gives 1 bit delay
+-- 40 gives  1 bit delay
+-- 80  gives  1 bit delay
+-- 120 gives 1 bit delay
+-- 125 gives 2 bit delay
+-- 130 gives 2 bit delay
+-- 140 gives 2 bit delay
+-- 160 gives 2 bit delay
+SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
+SIM_TAPDELAY_VALUE => 75) -- Per tap delay used for simulation in ps
+port map (
+--BUSY => BUSY, -- 1-bit output: Busy output after CAL
+DATAOUT => delayedDataPort(1), -- 1-bit output: Delayed data output to ISERDES/input register
+--DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
+DOUT => delayedDataForOutput(1), -- 1-bit output: Delayed data output
+--TOUT => TOUT, -- 1-bit output: Delayed 3-state output
+CAL => '0', -- 1-bit input: Initiate calibration input
+CE => '0', -- 1-bit input: Enable INC input
+CLK => '0', -- 1-bit input: Clock input
+IDATAIN => dataPort(1), -- 1-bit input: Data input (connect to top-level port or I/O buffer)
+INC => '0', -- 1-bit input: Increment / decrement input
+IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
+IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
+ODATAIN => fastWriteData(1),-- dataAssertedToOutput(1), -- 1-bit input: Output data input from output register or OSERDES2.
+RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
+T => slowdqsTristate -- 1-bit input: 3-state input signal
+);
 
 
 
 
+IODELAY2_inst2 : IODELAY2
+generic map (
+COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
+DATA_RATE => "SDR", -- "SDR" or "DDR"
+DELAY_SRC => "IO", -- "IO", "ODATAIN" or "IDATAIN"
+IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
+IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
+IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
+-- or "DIFF_PHASE_DETECTOR"
+IDELAY_VALUE => 40, -- Amount of taps for fixed input delay (0-255)
+ODELAY_VALUE => 40, -- Amount of taps fixed output delay (0-255)
+--IDELAY:
+-- 0 gives no change
+-- 60 gives no change
+-- 90 gives no change
+-- 95 gives no change
+-- 100 is flickering borderline 1 bit delay
+-- 105 gives 1 bit delay
+-- 120 gives 1 bit delay
+
+--ODELAY:
+-- 0 gives no change
+-- 1 gives 1 bit delay
+-- 5 gives 1 bit delay
+-- 10 gives 1 bit delay
+-- 20 gives 1 bit delay
+-- 40 gives  1 bit delay
+-- 80  gives  1 bit delay
+-- 120 gives 1 bit delay
+-- 125 gives 2 bit delay
+-- 130 gives 2 bit delay
+-- 140 gives 2 bit delay
+-- 160 gives 2 bit delay
+SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
+SIM_TAPDELAY_VALUE => 75) -- Per tap delay used for simulation in ps
+port map (
+--BUSY => BUSY, -- 1-bit output: Busy output after CAL
+DATAOUT => delayedDataPort(2), -- 1-bit output: Delayed data output to ISERDES/input register
+--DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
+DOUT => delayedDataForOutput(2), -- 1-bit output: Delayed data output
+--TOUT => TOUT, -- 1-bit output: Delayed 3-state output
+CAL => '0', -- 1-bit input: Initiate calibration input
+CE => '0', -- 1-bit input: Enable INC input
+CLK => '0', -- 1-bit input: Clock input
+IDATAIN => dataPort(2), -- 1-bit input: Data input (connect to top-level port or I/O buffer)
+INC => '0', -- 1-bit input: Increment / decrement input
+IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
+IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
+ODATAIN => fastWriteData(2),-- dataAssertedToOutput(2), -- 1-bit input: Output data input from output register or OSERDES2.
+RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
+T => slowdqsTristate -- 1-bit input: 3-state input signal
+);
 
 
 
+IODELAY2_inst3 : IODELAY2
+generic map (
+COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
+DATA_RATE => "SDR", -- "SDR" or "DDR"
+DELAY_SRC => "IO", -- "IO", "ODATAIN" or "IDATAIN"
+IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
+IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
+IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
+-- or "DIFF_PHASE_DETECTOR"
+IDELAY_VALUE => 40, -- Amount of taps for fixed input delay (0-255)
+ODELAY_VALUE => 40, -- Amount of taps fixed output delay (0-255)
+--IDELAY:
+-- 0 gives no change
+-- 60 gives no change
+-- 90 gives no change
+-- 95 gives no change
+-- 100 is flickering borderline 1 bit delay
+-- 105 gives 1 bit delay
+-- 120 gives 1 bit delay
 
-
-
-
-
-
-------------- Begin Cut here for INSTANTIATION Template ----- INST_TAG
-
--- User Notes:
--- None
-
-dataportsInstance : PhilPorts
-  port map
-   (
-  DATA_TO_AND_FROM_PINS =>   dataPort, --Bi-Directional Pins
-  DATA_IN_TO_DEVICE =>   delayedDataPort, --Output pins
-  -- From the device out to the system
-  DATA_OUT_FROM_DEVICE =>   fastWriteData, --Input pins
-
-  TRISTATE_OUTPUT =>   slowdqsTristate,  --Output pin
-  
--- Clock and reset signals
-  CLK_IN =>   clk250MHz,      -- Single ended clock from IOB
-  --CLK_OUT =>   CLK_OUT,    --Fast clock ouput
-  IO_RESET =>   slowFIFOrst);          --system reset
-
--- INST_TAG_END ------ End INSTANTIATION Template ------------
-
-
-
-
-
-
-
-
-
-
-
+--ODELAY:
+-- 0 gives no change
+-- 1 gives 1 bit delay
+-- 5 gives 1 bit delay
+-- 10 gives 1 bit delay
+-- 20 gives 1 bit delay
+-- 40 gives  1 bit delay
+-- 80  gives  1 bit delay
+-- 120 gives 1 bit delay
+-- 125 gives 2 bit delay
+-- 130 gives 2 bit delay
+-- 140 gives 2 bit delay
+-- 160 gives 2 bit delay
+SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
+SIM_TAPDELAY_VALUE => 75) -- Per tap delay used for simulation in ps
+port map (
+--BUSY => BUSY, -- 1-bit output: Busy output after CAL
+DATAOUT => delayedDataPort(3), -- 1-bit output: Delayed data output to ISERDES/input register
+--DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
+DOUT => delayedDataForOutput(3), -- 1-bit output: Delayed data output
+--TOUT => TOUT, -- 1-bit output: Delayed 3-state output
+CAL => '0', -- 1-bit input: Initiate calibration input
+CE => '0', -- 1-bit input: Enable INC input
+CLK => '0', -- 1-bit input: Clock input
+IDATAIN => dataPort(3), -- 1-bit input: Data input (connect to top-level port or I/O buffer)
+INC => '0', -- 1-bit input: Increment / decrement input
+IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
+IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
+ODATAIN => fastWriteData(3),-- dataAssertedToOutput(3), -- 1-bit input: Output data input from output register or OSERDES2.
+RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
+T => slowdqsTristate -- 1-bit input: 3-state input signal
+);
 
 
 
@@ -652,12 +799,11 @@ I => clkOutFast--clk125MHz -- Buffer input
 			nextCapturedData(11 downto 2) <= capturedData(10 downto 1);
 		end if;
 		
-		
 		if slowdqsTristate = '1' then
---			dataPort (15 downTo 0) <= (15 downTo 0 => 'Z');	
+			dataPort (15 downTo 0) <= (15 downTo 0 => 'Z');	
 		else
---			dataPort(3 downto 0) <= delayedDataForOutput(3 downto 0);
---			dataPort(15 downto 4) <= dataAssertedToOutput (15 downto 4);
+			dataPort(3 downto 0) <= delayedDataForOutput(3 downto 0);
+			dataPort(15 downto 4) <= dataAssertedToOutput (15 downto 4);
 		end if;
 		
 	end process;
