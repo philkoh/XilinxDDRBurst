5a72820e67ad70f8afc66300558ffed3dde8c549
GooseEnt <gooseenterprisesllc@gmail.com>
2022-02-02 02:19:06 -0500

    added second delay channel to Delay module

--- bl.vhd
+++ bl.vhd
@@ -625,66 +625,13 @@ Inst_Delay: Delay PORT MAP(
 		Tristate => slowdqsTristate
 	);
 
-	delayedDataPort(0) <= IncomingDelayedA(0);
-	delayedDataForOutput(0) <= OutgoingDelayedA(0);
-	IncomingUndelayedA(0) <= dataPort(0);
-	OutgoingUndelayedA(0) <= fastWriteData(0);
+	delayedDataPort(1 downto 0) <= IncomingDelayedA(1 downto 0);
+	delayedDataForOutput(1 downto 0) <= OutgoingDelayedA(1 downto 0);
+	IncomingUndelayedA(1 downto 0) <= dataPort(1 downto 0);
+	OutgoingUndelayedA(1 downto 0) <= fastWriteData(1 downto 0);
 
 
 
-
-IODELAY2_inst1 : IODELAY2
-generic map (
-COUNTER_WRAPAROUND => "WRAPAROUND", -- "STAY_AT_LIMIT" or "WRAPAROUND"
-DATA_RATE => "SDR", -- "SDR" or "DDR"
-DELAY_SRC => "IO", -- "IO", "ODATAIN" or "IDATAIN"
-IDELAY2_VALUE => 0, -- Delay value when IDELAY_MODE="PCI" (0-255)
-IDELAY_MODE => "NORMAL", -- "NORMAL" or "PCI"
-IDELAY_TYPE => "FIXED", -- "FIXED", "DEFAULT", "VARIABLE_FROM_ZERO", "VARIABLE_FROM_HALF_MAX"
--- or "DIFF_PHASE_DETECTOR"
-IDELAY_VALUE => 40, -- Amount of taps for fixed input delay (0-255)
-ODELAY_VALUE => 40, -- Amount of taps fixed output delay (0-255)
---IDELAY:
--- 0 gives no change
--- 60 gives no change
--- 90 gives no change
--- 95 gives no change
--- 100 is flickering borderline 1 bit delay
--- 105 gives 1 bit delay
--- 120 gives 1 bit delay
-
---ODELAY:
--- 0 gives no change
--- 1 gives 1 bit delay
--- 5 gives 1 bit delay
--- 10 gives 1 bit delay
--- 20 gives 1 bit delay
--- 40 gives  1 bit delay
--- 80  gives  1 bit delay
--- 120 gives 1 bit delay
--- 125 gives 2 bit delay
--- 130 gives 2 bit delay
--- 140 gives 2 bit delay
--- 160 gives 2 bit delay
-SERDES_MODE => "NONE", -- "NONE", "MASTER" or "SLAVE"
-SIM_TAPDELAY_VALUE => 75) -- Per tap delay used for simulation in ps
-port map (
---BUSY => BUSY, -- 1-bit output: Busy output after CAL
-DATAOUT => delayedDataPort(1), -- 1-bit output: Delayed data output to ISERDES/input register
---DATAOUT2 => DATAOUT2, -- 1-bit output: Delayed data output to general FPGA fabric
-DOUT => delayedDataForOutput(1), -- 1-bit output: Delayed data output
---TOUT => TOUT, -- 1-bit output: Delayed 3-state output
-CAL => '0', -- 1-bit input: Initiate calibration input
-CE => '0', -- 1-bit input: Enable INC input
-CLK => '0', -- 1-bit input: Clock input
-IDATAIN => dataPort(1), -- 1-bit input: Data input (connect to top-level port or I/O buffer)
-INC => '0', -- 1-bit input: Increment / decrement input
-IOCLK0 => '0', -- 1-bit input: Input from the I/O clock network
-IOCLK1 => '0', -- 1-bit input: Input from the I/O clock network
-ODATAIN => fastWriteData(1),-- dataAssertedToOutput(1), -- 1-bit input: Output data input from output register or OSERDES2.
-RST => '0', -- 1-bit input: Reset to zero or 1/2 of total delay period
-T => slowdqsTristate -- 1-bit input: 3-state input signal
-);
 
 
 
